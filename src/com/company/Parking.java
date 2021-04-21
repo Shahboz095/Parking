1075b8211630a6a09206c31fd4c86141d47e69c5
Shahboz095 <shahboz.17@mail.ru>
2021-04-21 17:30:45 +0500

    second

--- src/com/company/Parking.java
+++ src/com/company/Parking.java
@@ -13,7 +13,7 @@ public class Parking {
         int coud = 0;
         int command;
         while (true) {
-            System.out.println(" 1=>Enter car \n 2=>Exit car");
+            System.out.println(" 1=>Enter car \n 2=>Exit car \n 3=>All Money \n 4=> All cars \n 5=> Finish");
             command = scanner.nextInt();
             switch (command) {
                 case 1:
@@ -39,9 +39,29 @@ public class Parking {
                         park+=exit;
                         System.out.println("Exit car");
                         money+=exit*6000;
+                    }
+                    break;
+                case 3:
+                    System.out.println("All money" +money);
+                    break;
+                case 4:
+                    System.out.println( 30-park + "all cars");
+                    break;
+                case 5:
+                    if(30-park==0){
+                        System.out.println("bugun "+coud+"ta moshina kirdi");
+                        System.out.println("Jami mablag'i "+ money);
 
                     }
+                    if (30-park>0) {
+                        System.out.println(30 - park + "ta moshina Parkovkada qogan");
+                        System.out.println("Oldin moshinani chiqazvoring");
+                    }
+                    break;
             }
         }
+
+
+
     }
 }
